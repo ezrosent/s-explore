5db7754030678537456fa4e3dc2201595cf678cf
Eli Rosenthal <ezr@cs.brown.edu>
2016-04-25 17:08:25 -0400

    more cleanup

--- src/Recur2.hs
+++ src/Recur2.hs
@@ -3,40 +3,26 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE OverloadedStrings     #-}
 {-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE TypeOperators         #-}
+{-# LANGUAGE ViewPatterns          #-}
 module Recur2 where
--- Automate the mutual recursion seen in 'Recur' module
+-- Automate/generalize the mutual recursion seen in 'Recur' module
+-- Type definitions in 'Types.hs'
 
 import           Control.Lens
-import           Control.Monad                         (liftM)
+import           Control.Monad                    (liftM)
 import           Data.Data
-import           Data.Data.Lens                        (template)
-import qualified Data.Text                             as T
-import           GHC.Generics
+import           Data.Data.Lens                   (template)
+import qualified Data.Text                        as T
+import           GHC.Generics                     (Generic)
 import           Types
 import           Unbound.Generics.LocallyNameless
-import           Unbound.Generics.LocallyNameless.Bind (Bind (..))
-import           Unbound.Generics.LocallyNameless.Name (Name (..))
 
-type BindName a = Bind (Name a) a
+roL :: UnrollLeft (a :+: b) -> (a :+: b)
+roL = CR . Left
 
-mapname :: Name a -> (a -> b) -> Name b
-mapname (Fn x1 x2) _ = Fn x1 x2
-mapname (Bn x1 x2) _ = Bn x1 x2
-mapbind :: BindName a -> (a -> b) -> BindName b
-mapbind (B p t) f = B (mapname p f) (f t)
-
-type family Unroll a where
-  Unroll (Mu f) = f (Mu f)
-  Unroll (f a) = f (Unroll a)
-
-type family UnrollC a where
-  UnrollC (CR a b)     = Either (a (CR a b)) (b (CR a b))
-  UnrollC (Either a b) = Either (UnrollC a) (UnrollC b)
-
-instance Functor UT1 where
-  fmap f (Id a)  = Id (mapname a f)
-  fmap f (Lam a) = Lam (mapbind a f)
-  fmap f (App a1 a2) = App (f a1) (f a2)
+roR :: UnrollRight (a :+: b) -> (a :+: b)
+roR = CR . Right
 
 lam :: (Typeable a, Alpha a) => T.Text -> a -> UT1 a
 lam x y = Lam $ bind (s2n (T.unpack x)) y
@@ -58,38 +44,44 @@ injectL = CR . Left . fmap injectL . _unMu
 injectR :: Functor b => Mu b -> CR a b
 injectR = CR . Right . fmap injectR . _unMu
 
+projectL' :: Traversable a => (a (CR a b)) -> Maybe (a (Mu a))
+projectL' =  mapM (\case (CR (Left a))  -> Mu `liftM` projectL' a
+                         (CR (Right _)) -> Nothing)
+
+projectR' :: Traversable b => (b (CR a b)) -> Maybe (b (Mu b))
+projectR' =  mapM (\case (CR (Right a)) -> Mu `liftM` projectR' a
+                         (CR (Left _))  -> Nothing)
+
 projectL :: Traversable a => CR a b -> Maybe (Mu a)
-projectL = \case (CR (Left a)) -> Mu <$> (projectL' a)
+projectL = \case (CR (Left a)) -> Mu <$> projectL' a
                  _ -> Nothing
-  where projectL' :: Traversable a => (a (CR a b)) -> Maybe (a (Mu a))
-        projectL' =  mapM (\case (CR (Left a))  -> Mu `liftM` projectL' a
-                                 (CR (Right a)) -> Nothing)
 
 projectR :: Traversable b => CR a b -> Maybe (Mu b)
-projectR = \case (CR (Right a)) -> Mu <$> (projectR' a)
+projectR = \case (CR (Right a)) -> Mu <$> projectR' a
                  _ -> Nothing
-  where projectR' :: Traversable b => (b (CR a b)) -> Maybe (b (Mu b))
-        projectR' =  mapM (\case (CR (Right a))  -> Mu `liftM` projectR' a
-                                 (CR (Left a)) -> Nothing)
 
 doRewrite :: ((UnrollC U2) -> (UnrollC U2)) -> U2 -> U2
 doRewrite f u = u & (template :: Traversal' U2 (UnrollC U2)) %~ f
 
+dosub z y = runFreshM $ do
+  (x, body) <- unbind z
+  return $ subst x y body
+
 leftToRight :: U2 -> U2
 leftToRight = doRewrite $ \case
-                            Right (Lam2 x y) -> Left $ Lam $ bind (s2n x) y
-                            Right (App2 x y) -> Left $ App x y
-                            Right (Id2 x)    -> Left $ Id (s2n x)
-                            x                -> x
+  Right (Lam2 x y) -> Left $ Lam $ bind (s2n x) y
+  Right (App2 x y) -> Left $ App x y
+  Right (Id2 x)    -> Left $ Id $ s2n x
+  x                -> x
 
 rightToLeft :: U2 -> U2
 rightToLeft = doRewrite $ \case
-                            Left (Lam b)   -> runFreshM $ do
-                              (x, body) <- unbind b
-                              return $ Right $ Lam2 (name2String x) body
-                            Left (App x y) -> Right $ App2 x y
-                            Left (Id n)    -> Right $ Id2 (name2String n)
-                            x              -> x
+  Left (Lam b) ->runFreshM $ do
+    (x, body) <- unbind b
+    return $ Right $ Lam2 (name2String x) body
+  Left (App x y) -> Right $ App2 x y
+  Left (Id n)    -> Right $ Id2 $ name2String n
+  x              -> x
 
 -- Large number of type-class constraints, but these can genereally be discharged automatically
 -- (See Types.hs)
@@ -99,15 +91,17 @@ fuzz :: ( c ~ (CR UT1 a), r ~ (a c), l ~ (UT1 c)
          , Subst c r , Subst c l)
       => c -> c
 fuzz u = u & temp %~ (eta.liftBeta)
-  where temp :: (r ~ (a (CR UT1 a)), Typeable a, Data r, Generic r, Alpha r)
+  where temp :: ( r ~ (a (CR UT1 a))
+                , Typeable a
+                , Data r, Generic r, Alpha r)
              => Traversal' (CR UT1 a) (UT1 (CR UT1 a))
         temp = template
-        eta x = App (CR $ Left (Lam (bind (s2n "x") (CR $ Left x))))
-                    (CR $ Left (Lam (bind (s2n "x") (CR $ Left (Id (s2n "x"))))))
-        beta (App (CR (Left (Lam z))) y) = runFreshM $ (\(x, body) -> return $ subst x y body) =<< unbind z
-        beta x = CR (Left x)
-        liftBeta x = case beta x of (CR (Left z)) -> z
-                                    _             -> x
+        eta x = App (roL $ Lam $ bind (s2n "x") (roL x))
+                    (roL $ Lam $ bind (s2n "x") (roL $ Id $ s2n "x"))
+        beta (App (CR (Left (Lam z))) y) = dosub z y
+        beta x = roL x
+        liftBeta (beta -> CR (Left z)) = z
+        liftBeta x = x
 
 -- take an instance of CL1, perform some transformations on it, convert it back
 circuit :: Mu CL1 -> Maybe (Mu CL1)
--- src/Types.hs
+++ src/Types.hs
@@ -20,6 +20,7 @@
 module Types where
 import           Control.Lens
 import           Control.Lens.TH
+import           Data.Bifunctor.TH
 import           Data.Data
 import           Data.Data.Lens
 import qualified Data.Text                             as T
@@ -33,21 +34,23 @@ import           Unbound.Generics.LocallyNameless.Name (Name (..))
 newtype Mu (f :: * -> *)                = Mu  { _unMu :: f (Mu f) }
 makeLenses ''Mu
 
-
-
 newtype CR a b = CR { _unCR :: Either (a (CR a b)) (b (CR a b)) }
 makeLenses ''CR
 
 type a :+: b = CR a b
 --TODO: nice thing with Iso etc. from Lens
 
-
-
 data UT1 a = Id (Name a) | Lam (Bind (Name a) a) | App a a
   deriving (Show)
 
+instance Functor UT1 where
+  fmap f (Id a)  = Id $ fmap f a
+  fmap f (Lam a) = Lam $ bimap (fmap f) f a
+  fmap f (App a1 a2) = App (f a1) (f a2)
+
 data CL1 a = Id2 String | Lam2 String a | App2 a a
-  deriving (Eq, Ord, Show, Functor, Data, Typeable, Generic, Foldable, Traversable)
+  deriving ( Eq, Ord, Show, Functor, Data
+           , Typeable, Generic, Foldable , Traversable)
 
 makePrisms ''UT1
 makePrisms ''CL1
@@ -64,17 +67,19 @@ deriving instance Typeable (f (Mu f))            => Typeable (Mu f)
 deriving instance Generic (f (Mu f))             => Generic (Mu f)
 deriving instance (Typeable f, Data (f (Mu f)))  => Data (Mu f)
 instance (Generic (f (Mu f)) , Alpha (f (Mu f))) => Alpha (Mu f)
+
 -- (XXX: Orphan) instances for Name, Bind
 deriving instance (Data a) => Data (Name a)
 deriving instance (Data a) => Data (Bind (Name a) a)
+deriving instance Functor Name
+$(deriveBifunctor ''Bind)
 
 -- instances for UT1
 deriving instance (Typeable a) => Typeable (UT1 a)
 deriving instance (Generic a) => Generic (UT1 a)
 deriving instance (Data a) => Data (UT1 a)
 instance (Typeable a, Generic a, Alpha a) => Alpha (UT1 a)
-instance (Typeable a, Generic a, Alpha a) => Eq (UT1 a) where
-  (==) = aeq
+instance (Typeable a, Generic a, Alpha a) => Eq (UT1 a) where (==) = aeq
 
 -- I wonder if there is a way to derive this usefully. It does not use any knowledge beyond the
 -- Original 'Subst' instance
@@ -99,7 +104,7 @@ instance Subst (CR UT1 CL1) (UT1 (CR UT1 CL1)) where
                                                     CR (Right _) -> Nothing
   isCoerceVar _      = Nothing
 
--- instances 'UT'
+-- Instances for 'UT'
 instance Subst UT (UT1 UT) where
   isCoerceVar (Id a) = Just $ SubstCoerce a (Just . _unMu)
   isCoerceVar _      = Nothing
@@ -108,6 +113,24 @@ instance Subst UT (UT1 UT) => Subst UT UT where
   isvar (Mu (Id a)) = Just (SubstName a)
   isvar _           = Nothing
 
+-- Useful type families
+type family Unroll a where
+  Unroll (Mu f) = f (Mu f)
+  Unroll (f a) = f (Unroll a)
+
+type family UnrollC a where
+  UnrollC (CR a b)     = Either (a (CR a b)) (b (CR a b))
+  UnrollC (Either a b) = Either (UnrollC a) (UnrollC b)
+
+type family UnrollLeft a where
+  UnrollLeft (CR a b)  = a (CR a b)
+  UnrollLeft (a c)     = a (a c)
+
+type family UnrollRight a where
+  UnrollRight (CR a b)  = b (CR a b)
+  UnrollRight (a c)     = a (a c)
+
+-- Instances for 'CR'
 -- 'CRHas' means that 'c' holds for (CR a b) unrolled in both directions
 type CRHas (c :: * -> Constraint) a b = (c (a (a :+: b)), c (b (a :+: b)))
 
